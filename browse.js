dc883785dbb45ec57fef5f8a37853bdab78d659a
Yuvraj Singh <yuvrajeyes@gmail.com>
2021-09-25 13:46:16 +0530

    addded browse.js

--- browse.js
+++ browse.js
@@ -1,12 +1,22 @@
 let browse = document.getElementById("browse");
-// browse.classList.add("low-opacity");
-// console.log(browse);
-let closeCard = document.querySelector(".closeicon");
-let showCard = document.querySelectorAll(".drama-item");
+let vdo = document.querySelector("#video-container");
+let plays = document.querySelectorAll(".play-btn");
+let closeCard = document.querySelectorAll(".closeicon");
+let showCard = document.querySelectorAll(".card-item");
 
-closeCard.addEventListener("click", closeItem);
+// Initially hide the video
+vdo.style.display = "none";
+
+closeCard.forEach((card) => {
+  card.addEventListener("click", closeItem);
+});
+
 showCard.forEach((card) => {
   card.addEventListener("click", showItem);
+});
+
+plays.forEach((play) => {
+  play.addEventListener("click", playVideo);
 });
 
 function closeItem(e) {
@@ -22,3 +32,26 @@ function showItem(e) {
   // Add show-card class
   selectedItem.classList.add("show-card");
 }
+
+function playVideo(e) {
+  // blur whole page when video pop-up
+  browse.classList.add("blur_2px");
+  // show video
+  vdo.style.display = "block";
+}
+
+window.onload = function () {
+  document.ondblclick = function (e) {
+    if (e.target.id !== "video-container") {
+      // set video_container's display equal to none
+      vdo.style.display = "none";
+      // pause video
+      vdo.children[0].pause();
+      // set current time to zero
+      vdo.children[0].currentTime = 0;
+      // remove blur_2px class
+      browse.classList.remove("blur_2px");
+    }
+  };
+};
+
